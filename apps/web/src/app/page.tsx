345a2bf08e74e881287e12dfdc93d1f80c91dd29
Rajdeep Das <rajdeepds626@gmail.com>
2025-05-18 05:29:10 +0000

    intial setup

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,3 +1,7 @@
-export default function Home() {
-  return <div>Hello World</div>;
+export default async function Home() {
+  const response = await fetch("http://localhost:8000/");
+  const data = await response.json();
+
+  console.log(data);
+  return <div>{data.message}</div>;
 }
